b8349a84bd230b38e76c94baf482dbb29a341824
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-08-04 21:50:07 -0500

    routes bug fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'users#index'
-  root 'application'
+  root 'application#hello'
 end
